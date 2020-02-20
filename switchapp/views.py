58f59448e01306572427f64bcffb7b4fe35459a8
Syazwan <sazwanazlan@gmail.com>
2020-02-20 23:29:27 +0800

    add dashboard content

--- switchapp/views.py
+++ switchapp/views.py
@@ -13,7 +13,6 @@ def index(request):
 	#update switch status and created at column 
 	if switches.exists():
 		for sw in switches.iterator():
-
 			if sw.switch_status is None:
 				if(sw.t1==0 and sw.t2==0 and sw.t3==0 and sw.t4==0 and sw.t5==0):
 					Switch.objects.filter(pk=sw.id).update(switch_status=0)
@@ -21,8 +20,64 @@ def index(request):
 					Switch.objects.filter(pk=sw.id).update(switch_status=1)
 			if sw.created_at is None:
 				Switch.objects.filter(pk=sw.id).update(created_at=sw.timestamp)
+	#end update
+	#optional function
+	#count switch 1
+	switch1 = Switch.objects.filter(name='S1')
+	switch1_count = Switch.objects.filter(name='S1').count()
+	count_sw1_success = 0
+	count_sw1_failed = 0
+	for cs in switch1.iterator():
+		if cs.switch_status == 1:
+			count_sw1_success = count_sw1_success + 1
+		if cs.switch_status == 0:
+			count_sw1_failed = count_sw1_failed + 1
+
+	avg_sw1_success = 0
+	avg_sw1_success = count_sw1_success / switch1_count * 100
+	avg_sw1_failed = 0
+	avg_sw1_failed = count_sw1_failed / switch1_count * 100
+
+	#count switch 2
+	switch2 = Switch.objects.filter(name='S2')
+	switch2_count = Switch.objects.filter(name='S2').count()
+	count_sw2_success = 0
+	count_sw2_failed = 0
+	for cs in switch2.iterator():
+		if cs.switch_status == 1:
+			count_sw2_success = count_sw2_success + 1
+		if cs.switch_status == 0:
+			count_sw2_failed = count_sw2_failed + 1
+
+	avg_sw2_success = 0
+	avg_sw2_success = count_sw2_success / switch2_count * 100
+	avg_sw2_failed = 0
+	avg_sw2_failed = count_sw2_failed / switch2_count * 100
+
+	#count switch 3
+	switch3 = Switch.objects.filter(name='S3')
+	switch3_count = Switch.objects.filter(name='S3').count()
+	count_sw3_success = 0
+	count_sw3_failed = 0
+	for cs in switch3.iterator():
+		if cs.switch_status == 1:
+			count_sw3_success = count_sw3_success + 1
+		if cs.switch_status == 0:
+			count_sw3_failed = count_sw3_failed + 1
+
+	avg_sw3_success = 0
+	avg_sw3_success = count_sw3_success / switch3_count * 100
+	avg_sw3_failed = 0
+	avg_sw3_failed = count_sw3_failed / switch3_count * 100
+
+	contex = {"title":"Home",
+			  "count_sw1_success": count_sw1_success, "count_sw1_failed":count_sw1_failed, "avg_sw1_success": round(avg_sw1_success), "avg_sw1_failed": round(avg_sw1_failed),
+			  "count_sw2_success": count_sw2_success, "count_sw2_failed":count_sw2_failed, "avg_sw2_success": round(avg_sw2_success), "avg_sw2_failed": round(avg_sw2_failed),
+			  "count_sw3_success": count_sw3_success, "count_sw3_failed":count_sw3_failed, "avg_sw3_success": round(avg_sw3_success), "avg_sw3_failed": round(avg_sw3_failed),
+			  }
+
 	
-	return render(request, 'index.html', {'title':"Home"})
+	return render(request, 'index.html', contex)
 
 def chart(request):
 
